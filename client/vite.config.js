5b9493923b9690b645f1c0968d6f4c0dfe814ae9
Manojkamriya <kamriyamanoj45@gmail.com>
2025-10-07 13:57:11 +0530

    vite config updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,20 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 5000,
-    allowedHosts: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3002',
-        changeOrigin: true
-      },
-      '/socket.io': {
-        target: 'http://localhost:3002',
-        ws: true,
-        changeOrigin: true
-      }
-    }
-  }
+  base: './'  
 })
